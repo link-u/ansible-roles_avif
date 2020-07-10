592b4aee9f4418de57f0ca949803a50afb12385c
y-hashida <yoshimasa.hashida@link-u.co.jp>
2020-07-10 23:20:06 +0900

    update: パッケージがインストール済みであるテストを記述.

--- molecule/common/tests/test_default.py
+++ molecule/common/tests/test_default.py
@@ -5,5 +5,6 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-def test_dummy(host):
-    assert True
+def test_installed_cavif_and_davif(host):
+    assert host.package("cavif").is_installed
+    assert host.package("davif").is_installed
