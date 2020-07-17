9460770da04563703f2434837600794ccf215824
y-hashida <yoshimasa.hashida@link-u.co.jp>
2020-07-18 02:20:14 +0900

    fix: リポジトリの追加テストの修正.

--- molecule/common/tests/test_repository.py
+++ molecule/common/tests/test_repository.py
@@ -5,11 +5,14 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 def test_added_repository_pkg_link_u_co_jp(host):
+    ## gpg コマンドのインストールテスト
+    assert host.package("gpg").is_installed
+
     ## GPG キーのインストールテスト
     assert not host.ansible(
         "apt_key",
-        "id=4DE76DC836A27DBAE17FAC4B09C9B9C18F429AAE" \
-        "url=https://pkg.link-u.co.jp/key.asc" \
+        "id=4DE76DC836A27DBAE17FAC4B09C9B9C18F429AAE " + \
+        "url=https://pkg.link-u.co.jp/key.asc " + \
         "state=present")["changed"]
 
     ## link-u apt リポジトリのインストールテスト
