b1dac1c2ea0ffc8b9c7340554926db9eb5a73cf1
y-hashida <yoshimasa.hashida@link-u.co.jp>
2020-07-18 04:13:55 +0900

    update: test の関数名を変更

--- molecule/common/tests/test_default.py
+++ molecule/common/tests/test_default.py
@@ -4,10 +4,10 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-def test_installed_cavif_and_davif(host):
+def test_installed_package(host):
     assert host.package("cavif").is_installed
     assert host.package("davif").is_installed
 
-def test_run_help_cavif_and_davif(host):
+def test_run_help_command(host):
     assert host.run("cavif --help").succeeded
     assert host.run("davif --help").succeeded
